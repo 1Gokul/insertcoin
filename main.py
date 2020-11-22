f2c0134b65b00b378cdb6e58d5eec2569ee4ee2e
Gokul Viswanath <viswanath1gokul@gmail.com>
2020-11-23 00:34:25 +0530

    Fixed login and logout bugs
    Now the user will get redirected if they try to access an account page after signing out.
    Redirects after successful login will be next.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from flask_pymongo import pymongo
 import secrets
 import string
 import os
+import functools
+
 
 CONNECTION_STRING = os.environ.get("MONGO_KEY")
 client = pymongo.MongoClient(CONNECTION_STRING)
@@ -11,6 +13,7 @@ db = client['GameRampStore']
 
 app = Flask(__name__)
 app.secret_key = secrets.token_urlsafe(32)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 bcrypt = Bcrypt(app)
 
 # default vars for flask
@@ -24,31 +27,43 @@ def home_redirect():
     return redirect(url_for('index'))
 
 
-@app.route('/account')
-def acc_check():
-    global account
-    if 'username' in session:
-        account = session['username'].upper()
-        return render_template('profile.html', account=account)
+def checkif_loggedin(func):
+    @functools.wraps(func)
+    def secure_function(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for("login", next=request.url))
+        return func(*args, **kwargs)
 
-    return render_template('login.html', account=account)
+    return secure_function
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    return response
+
+@app.route('/account')
+@checkif_loggedin
+def acc_check():
+
+    if 'username' in session:
+
+        return render_template('library.html',
+                               account=session['username'].upper())
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/home')
 def index():
-    global account
     if 'username' in session:
-        account = session['username'].upper()
-        return render_template('library.html', account=account)
+        return render_template('library.html',
+                               account=session['username'].upper())
+    else:
+        return render_template('home.html', account="Log In")
 
-    return render_template('home.html', account=account)
-
-
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-
     if request.method == 'POST':
-        global account
         users = db['UserInfo']
         login_user = users.find_one({'username': request.form['username']})
 
@@ -60,12 +75,16 @@ def login():
                 return redirect(url_for('index'))
         else:
             error = "The username or password that you have entered is incorrect."
-            return render_template('login.html', account=account, error=error)
+            return render_template('login.html',
+                                   account=session['username'].upper(),
+                                   error=error)
+
+    else:
+        return render_template('login.html', account="Log In")
 
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    global account
 
     if request.method == 'POST':
 
@@ -88,24 +107,24 @@ def register():
                 # session['username'] = request.form['username']
                 return render_template(
                     'login.html',
-                    account=account,
+                    account=session['username'].upper(),
                     comment='Your account was added successfully!')
 
             else:
                 error = "The username that you entered already exists."
                 return render_template('register.html',
-                                       account=account,
+                                       account=session['username'].upper(),
                                        error=error)
 
         else:
             error = "The passwords you entered do not match."
             return render_template('register.html',
-                                   account=account,
+                                   account=session['username'].upper(),
                                    error=error)
 
     else:
         return render_template('register.html',
-                               account=account,
+                               account=session['username'].upper(),
                                acclink=loginlink)
 
 
@@ -116,106 +135,96 @@ def logout():
 
 
 @app.route('/store')
+@checkif_loggedin
 def store():
-    global account
-    if 'username' in session:
-        account = session['username'].upper()
-        return render_template('store.html', account=account)
-
-    return redirect(url_for('index'))
+    return render_template('store.html',
+                               account=session['username'].upper())
 
 
 @app.route('/profile')
+@checkif_loggedin
 def profile():
-    global account
     global db
-    if 'username' in session:
-        account = session['username'].upper()
-        userinfo = db['UserInfo']
-        user = userinfo.find_one({"username": session['username']})
-
-        if user:
-            return render_template('profile.html',
-                                   account=account,
-                                   username=user['username'],
-                                   prof=user['profilename'],
-                                   games=user['games'],
-                                   ngames=len(user['games']),
-                                   balance=user['balance'])
-        else:
-            return redirect(url_for('index'))
-    else:
-        return redirect(url_for('index'))
+    user = db['UserInfo'].find_one({"username": session['username']})
+    return render_template('profile.html',
+                            account=session['username'].upper(),
+                            username=user['username'],
+                            prof=user['profilename'],
+                            games=user['games'],
+                            ngames=len(user['games']),
+                            balance=user['balance'])
 
 
 @app.route('/library')
+@checkif_loggedin
 def library():
-    global account
-    if 'username' in session:
-        account = session['username'].upper()
-        return render_template('library.html', account=account)
 
-    return redirect(url_for('index'))
+    return render_template('library.html',
+                               account=session['username'].upper())
 
 
 @app.route('/about')
+@checkif_loggedin
 def about():
-    global account
+
     if 'username' in session:
-        account = session['username'].upper()
-        return render_template('about.html', account=account, layout='layout')
+
+        return render_template('about.html',
+                               account=session['username'].upper(),
+                               layout='layout')
     else:
-        return render_template('about.html', account=account, layout='home')
+        return render_template('about.html',
+                               account=session['username'].upper(),
+                               layout='home')
 
 
 @app.route('/accountsettings')
 def acc_settings():
 
-    global account
-    if 'username' in session:
-        account = session['username'].upper()
-        return render_template('accountsettings.html', account=account)
-
-    else:
-        return redirect(url_for('index'))
+    return render_template('accountsettings.html',
+                               account=session['username'].upper())
 
 
 @app.route('/changeusername', methods=['POST', 'GET'])
+@checkif_loggedin
 def changeusername():
-    global account
-    if 'username' in session:
-        account = session['username'].upper()
 
-        if request.method == 'POST':
-            userinfo = db['UserInfo']
-            user = userinfo.find_one({"username": request.form['oldusername']})
+    if request.method == 'POST':
+        userinfo = db['UserInfo']
+        user = userinfo.find_one({"username": request.form['oldusername']})
 
-            if user:
-                existing_user = userinfo.find_one(
-                    {"username": request.form['newusername']})
-                if existing_user:
-                    return render_template('accountsettings.html', account=account, error="The New Username already exists!")
-                else:
-                    userinfo.update_one({'username': request.form['oldusername']}, {
-                                        "$set": {"username": request.form['newusername']}})
-                    session['username'] = request.form['newusername']
-                    account = session['username'].upper()
-                    return render_template('accountsettings.html', account=account, comment="New Username set successfully!")
-
+        if user:
+            existing_user = userinfo.find_one(
+                {"username": request.form['newusername']})
+            if existing_user:
+                return render_template(
+                    'accountsettings.html',
+                    account=session['username'].upper(),
+                    error="The New Username already exists!")
             else:
-                return render_template('accountsettings.html', account=account, error="The Old username is incorrect.")
-    else:
-        return redirect(url_for('index'))
+                userinfo.update_one(
+                    {'username': request.form['oldusername']},
+                    {"$set": {
+                        "username": request.form['newusername']
+                    }})
+                session['username'] = request.form['newusername']
 
+                return render_template(
+                    'accountsettings.html',
+                    account=session['username'].upper(),
+                    comment="New Username set successfully!")
+
+        else:
+            return render_template('accountsettings.html',
+                                    account=session['username'].upper(),
+                                    error="The Old username is incorrect.")
 
 @app.route('/changepassword', methods=['POST', 'GET'])
+@checkif_loggedin
 def changepassword():
-    global account
-    if 'username' in session:
-        account = session['username'].upper()
-        return render_template('library.html', account=account)
 
-    return redirect(url_for('index'))
+    return render_template('library.html',
+                               account=session['username'].upper())
 
 
 if __name__ == '__main__':
